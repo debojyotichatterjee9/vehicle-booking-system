ed380feb3609e9c91544cf7e39ff933c3bfae9ef
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2023-03-01 20:32:57 +0000

    Merged in feature/fastify-session (pull request #1)
    Feature/fastify session
    
    * feat:signin progress
    
    * feat:working with fastify/session
    
    * feat:basic session work done

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,33 @@ import {
 } from '@nestjs/platform-fastify';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app/app.module';
+const cookieSession = require('cookie-session');
+import type { FastifyCookieOptions } from '@fastify/cookie';
+import cookie from '@fastify/cookie'
+import { fastifySession } from '@fastify/session';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: true })
   );
+  // Using cookie-session
+  app.register(cookie, {
+    secret: "68e109f0f40ca72a15e05cc22786f8e6", // for cookies signature
+    parseOptions: {}     // options for parsing cookies
+  } as FastifyCookieOptions)
+  app.register(fastifySession, {
+    secret: '68e109f0f40ca72a15e05cc22786f8e6',
+    cookie: {
+      path: "/",
+      maxAge: 3600000,
+      httpOnly: false,
+      secure: "auto",
+      sameSite: true,
+      // domain: "localhost"
+    }
+  });
+  // using Validation Pipe
   app.useGlobalPipes(new ValidationPipe({
     whitelist: false
   }));
