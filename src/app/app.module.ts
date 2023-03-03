43593d0cc6c130ead3921647c48266737a993a52
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2023-03-04 01:36:01 +0530

    feat:added config files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { UsersModule } from "../modules/users/users.module";
@@ -9,22 +10,49 @@ import { User } from "src/modules/users/users.entity";
 import { VehicleCategory, VehicleType } from "src/modules/vehicles/vehicles.entity";
 import { FixedTariff } from "src/modules/tariff/tarriff.entity";
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "root",
-    password: "password",
-    database: "postgres",
-    cache: {
-      duration: 30000 // keeping cache 30 seconds
-    },
-    synchronize: true,
-    logging: true,
-    entities: [User, VehicleCategory, VehicleType, FixedTariff],
-    subscribers: [],
-    migrations: [],
-  }), UsersModule, VehiclesModule, TariffModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: config.get<string>('DB_DRIVER'),
+          host: config.get<string>('DB_HOST'),
+          port: config.get<number>('DB_PORT'),
+          username: config.get<string>('DB_USERNAME'),
+          password: config.get<string>('DB_PASSWORD'),
+          database: config.get<string>('DB_NAME'),
+          cache: {
+            duration: config.get<number>('DB_CACHE_DURATION') // keeping cache 30 seconds
+          },
+          synchronize: config.get<boolean>('DB_SYNC'),
+          logging: config.get<boolean>('DB_LOGGING'),
+          entities: [User, VehicleCategory, VehicleType, FixedTariff],
+          subscribers: [],
+          migrations: [],
+        } as TypeOrmModuleOptions
+      }
+    }),
+    //   TypeOrmModule.forRoot({
+    //   type: "postgres",
+    //   host: "localhost",
+    //   port: 5432,
+    //   username: "root",
+    //   password: "password",
+    //   database: "postgres",
+    //   cache: {
+    //     duration: 30000 // keeping cache 30 seconds
+    //   },
+    //   synchronize: true,
+    //   logging: true,
+    //   entities: [User, VehicleCategory, VehicleType, FixedTariff],
+    //   subscribers: [],
+    //   migrations: [],
+    // }), 
+    UsersModule, VehiclesModule, TariffModule],
   controllers: [AppController],
   providers: [AppService],
 })
